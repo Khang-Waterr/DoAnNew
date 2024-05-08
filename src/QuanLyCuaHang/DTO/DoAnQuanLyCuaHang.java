ebcd1185c9d96a357260f9476db32b815f1fb45c
nguye <nguye@No3ra>
2024-05-08 21:24:29 +0700

    LIB

---
+++ src/QuanLyCuaHang/DTO/DoAnQuanLyCuaHang.java
@@ -0,0 +1,11 @@
+package QuanLyCuaHang.DTO;
+
+import doanquanlycuahang.*;
+
+public class DoAnQuanLyCuaHang {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/doanquanlycuahang/DoAnQuanLyCuaHang.java
@@ -0,0 +1,9 @@
+package doanquanlycuahang;
+
+public class DoAnQuanLyCuaHang {
+
+    public static void main(String[] args) {
+         System.out.println("Hello World!");
+    }
+    
+}
