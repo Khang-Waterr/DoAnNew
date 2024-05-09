4283fb44927ec6d82f096a2490ddba38ae884e97
nguye <nguye@No3ra>
2024-05-09 08:52:51 +0700

    MacDoneld

--- src/doanquanlycuahang/DoAnQuanLyCuaHang.java
+++ src/doanquanlycuahang/DoAnQuanLyCuaHang.java
@@ -1,9 +1,14 @@
 package doanquanlycuahang;
-
+import QuanLyCuaHang.GUI.KhachHangGUI;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DoAnQuanLyCuaHang {
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
          System.out.println("Hello World!");
+         new KhachHangGUI().setVisible(true);
+         
     }
     
 }
