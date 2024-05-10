d17133c3a7c59bc13edfb8d4f9f9b70deb4656f0
nguye <nguye@No3ra>
2024-05-10 07:37:04 +0700

    MyConnect

---
+++ src/doanquanlycuahang/MyConnect.java
@@ -0,0 +1,30 @@
+package doanquanlycuahang;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnect {
+    public Connection con = null;
+    public String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+    public String uname = "QLCH";
+    public String upass = "QLCH";
+    public MyConnect(){
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            System.out.println("Oracle JDBC Driver Registered!");
+            con = DriverManager.getConnection(url, uname, upass);
+            if (con != null) { 
+                System.out.println("Connected to the database"); 
+            } 
+            else { 
+                System.out.println("Failed to make connection!"); }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Oracle JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+        }
+    }
+}
